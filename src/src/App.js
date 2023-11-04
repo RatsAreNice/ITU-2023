9857936e5da7899815644800a5811c879432d97b
Oliver Nemcek <olivernemcek7@gmail.com>
2023-11-04 23:15:40 +0100

    kostra aplikacie

---
+++ src/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home'
+import NajistUdalosti from './NajistUdalosti';
+import Profile from './Profile';
+
+function App() {
+  
+
+  return (
+    <Router>
+      <div className="App">
+        <div className='Content'>
+          <Routes>
+            <Route exact path='/' element={< Home />}></Route>
+            <Route exact path='/najist' element={< NajistUdalosti />}></Route>
+            <Route exact path='/profile' element={< Profile />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/src/Profile.js
@@ -0,0 +1,55 @@
+import './App.css';
+import { useState} from "react";
+
+
+
+
+function Profile() {
+
+    var [event, setEvent] = useState(null);
+
+  function handleClick(){
+    fetch('http://eva.fit.vutbr.cz/~xdobia15/')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      setEvent(data);
+    })
+  }
+  
+
+  function printdata(){
+    if(event != null){
+      return <div><pre>{JSON.stringify(event, null, 2) }</pre></div>;
+    }
+    return "";
+  }
+
+
+  return (
+  <div className='Body'>
+          <div class="buttons">
+              <div class="action_btn">
+                  <a href="/">
+                      <button>Moje Udalosti</button>
+                  </a>
+                  <a href="/najist">
+                      <button>Najist Udalost</button>
+                  </a>
+                  <p id="saved"></p>
+              </div>
+          </div>
+          <br/>
+          <div className='Title'><h1>Profil</h1>
+              <div className='Button'>
+                  <button onClick={handleClick}>Načítať udalosť</button> 
+                  { printdata() }
+              </div>
+          </div>
+      </div>
+  );
+}
+
+export default Profile;
