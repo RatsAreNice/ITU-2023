9cc0765e84b8a667ae3e790aff2d04b00b8edede
DontBanMeAgain <xpacka00@stud.fit.vutbr.cz>
2023-12-17 12:15:17 +0100

    ja ta mozem

--- src/src/PonukaPreview.js
+++ src/src/PonukaPreview.js
@@ -1,59 +1,90 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
-const PonukaPreview = ( {AuthUser, ponuka, fetchAgain, setFetchagain} ) => {
-    const [detail, setDetail] = useState(false);
 
-    let vlastnik = false;
+const PonukaPreview = ({ AuthUser, ponuka, fetchAgain, setFetchagain }) => {
+  const [detail, setDetail] = useState(false);
 
-    if(AuthUser){
-        if(AuthUser.email === ponuka.user){
-            vlastnik = true;
-        }
-    }
+  let vlastnik = AuthUser && AuthUser.email === ponuka.user;
 
-    const zobrazitDetail = () => {
-        setDetail(true);
-    }
+  const handleToggleDetail = () => {
+    setDetail(!detail);
+  };
 
-    const skrytDetail = () => {
-        setDetail(false);
-    }
+  const handleDelete = () => {
+    fetch(`http://localhost:8000/ponuka/${ponuka.id}`, {
+      method: "DELETE",
+    }).then(() => {
+      setFetchagain(fetchAgain + 1);
+      // If you have a routing setup, you might want to navigate back to the list.
+      // navigate('/');
+    });
+  };
 
-    const handleDelete = () => {
-        fetch('http://localhost:8000/ponuka/' + ponuka.id, {
-            method: "DELETE"
-        }).then(() => {
-            setFetchagain(fetchAgain + 1);
-            //navigate('/')
-        }) 
-    }
+  // Inline styles
+  const styles = {
+    card: {
+      backgroundColor: '#fff',
+      borderRadius: '8px',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+      padding: '20px',
+      marginBottom: '10px',
+      textAlign: 'center',
+    },
+    button: {
+      display: 'block',
+      margin: '10px auto',
+      padding: '8px 16px',
+      backgroundColor: '#007bff',
+      color: 'white',
+      border: 'none',
+      borderRadius: '4px',
+      cursor: 'pointer',
+    },
+    deleteButton: {
+      backgroundColor: '#dc3545',
+    },
+    editButton: {
+      textDecoration: 'none',
+      backgroundColor: '#28a745',
+      color: 'white',
+    },
+    detail: {
+      marginTop: '15px',
+      borderTop: '1px solid #eee',
+      paddingTop: '15px',
+    },
+  };
 
-    return ( 
-        <div className="ponuka-preview" key={ponuka.id} >
-            <h1>Ponuka { ponuka.nazov } </h1>
-            <h2>Cena: { ponuka.cena }</h2>
+  return (
+    <div style={styles.card}>
+      <h1>Ponuka {ponuka.nazov}</h1>
+      <h2>Cena: {ponuka.cena}</h2>
 
-            {!detail && <button onClick={zobrazitDetail}> Detail </button>}
-            {detail && <button onClick={skrytDetail}> Skryt detail </button>}
+      <button onClick={handleToggleDetail} style={styles.button}>
+        {detail ? 'Skryt detail' : 'Detail'}
+      </button>
 
-            {detail && 
-                <div className="detail">
-                <h2>Popis: { ponuka.popis }</h2>
-                <h2>Od { ponuka.user }</h2>
-                <div className="obrazok">
-                    <img src={ ponuka.image } alt="" />
-                </div>
-                </div>
-            }
-            { vlastnik && 
-            <div className="buttons">
-                <button onClick={handleDelete}> Odstranit ponuku </button>
-                <Link to={`/ponuka/${ponuka.id}/upravit`}><button>Upravit ponuku</button></Link>
-            </div>
-            }
+      {detail && (
+        <div style={styles.detail}>
+          <p>Popis: {ponuka.popis}</p>
+          <p>Od: {ponuka.user}</p>
+          {ponuka.image && <img src={ponuka.image} alt={`Obrazok ${ponuka.nazov}`} />}
         </div>
-     );
-}
- 
+      )}
+
+      {vlastnik && (
+        <div>
+          <button onClick={handleDelete} style={{ ...styles.button, ...styles.deleteButton }}>
+            Odstranit ponuku
+          </button>
+          <Link to={`/ponuka/${ponuka.id}/upravit`} style={{ ...styles.button, ...styles.editButton }}>
+            Upravit ponuku
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default PonukaPreview;
--- src/src/Trhovisko.js
+++ src/src/Trhovisko.js
@@ -4,37 +4,81 @@ import { useState } from 'react';
 import PonukaPreview from './PonukaPreview';
 import VytvoritPonuku from './VytvoritPonuku';
 
+function Home({ AuthUser }) {
+  const [fetchAgain, setFetchagain] = useState(0);
+  const { data, isPending, error } = useFetch('http://localhost:8000/ponuka', fetchAgain);
+  const [vytvorenie, setVytvorenie] = useState(false);
 
-function Home( {AuthUser} ) {
-  const [fetchAgain, setFetchagain] = useState(0)
-  const { data, isPending, Error} = useFetch('http://localhost:8000/ponuka', fetchAgain)
-  const [vytvorenie, setVytvorenie] = useState(false)
-
-  const showVytvorenie = () => {
-    setVytvorenie(true)
-  }
-
-  const hideVytvorenie = () => {
-    setVytvorenie(false)
-  }
+  const styles = {
+    trhovisko: {
+      padding: '20px',
+      paddingBottom: '60px', 
+      paddingTop: '60px', 
+      minHeight: 'calc(100vh - 60px)',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      width: '100vw',
+    },
+    button: {
+      backgroundColor: '#4CAF50',
+      color: 'white',
+      padding: '10px 20px',
+      border: 'none',
+      borderRadius: '4px',
+      cursor: 'pointer',
+      fontSize: '18px',
+      margin: '20px 0',
+    },
+    udalosti: {
+      width: '100%',
+      maxWidth: '960px',
+      display: 'grid',
+      gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))',
+      gap: '20px',
+      padding: '20px',
+    },
+    loading: {
+      color: '#ffffff',
+    },
+    error: {
+      color: '#ff0000',
+      backgroundColor: 'rgba(255, 0, 0, 0.1)',
+      padding: '10px',
+      borderRadius: '4px',
+    },
+  };
 
   return (
-    <div className="trhovisko">
-    {!vytvorenie && <button onClick={showVytvorenie}>Vytvorit ponuku</button>}
-      {vytvorenie && <button onClick={hideVytvorenie}>Vytvorit ponuku</button>}
+    <div style={styles.trhovisko}>
+      <button onClick={() => setVytvorenie(!vytvorenie)} style={styles.button}>
+        {vytvorenie ? 'Skryt formulár' : 'Vytvorit ponuku'}
+      </button>
 
-      {vytvorenie && <VytvoritPonuku AuthUser={AuthUser} fetchAgain={fetchAgain} setFetchagain={setFetchagain} />}
-      { Error && <div>{ Error }</div> }
-      { isPending && <div>Loading...</div> }
-      { data &&
-      <div className="udalosti">
-        {data.map((ponuka) => (
-          <div className="ponuky" key={ponuka.id} >
-            <PonukaPreview AuthUser={AuthUser} ponuka={ponuka} fetchAgain={fetchAgain} setFetchagain={setFetchagain} />
-          </div>
-        ))
-        }
-      </div>}
+      {vytvorenie && (
+        <VytvoritPonuku
+          AuthUser={AuthUser}
+          fetchAgain={fetchAgain}
+          setFetchagain={setFetchagain}
+        />
+      )}
+
+      {error && <div style={styles.error}>{error}</div>}
+      {isPending && <div style={styles.loading}>Načítava sa...</div>}
+      {data && (
+        <div style={styles.udalosti}>
+          {data.map((ponuka) => (
+            <PonukaPreview
+              key={ponuka.id}
+              AuthUser={AuthUser}
+              ponuka={ponuka}
+              fetchAgain={fetchAgain}
+              setFetchagain={setFetchagain}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
