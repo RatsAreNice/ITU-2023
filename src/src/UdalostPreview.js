6b5a3545df48fde10a3279974fc67329f2ab78be
xdobia15 - Desktop <xdobia15@vutbr.cz>
2023-12-17 08:18:28 +0100

    uprimne asi sa odjebem uz fakt

--- src/src/UdalostPreview.js
+++ src/src/UdalostPreview.js
@@ -2,25 +2,41 @@ import { Link } from "react-router-dom";
 import ZaujemButton from "./ZaujemButton";
 import { useEffect, useState } from "react";
 
-const UdalostPreview = ( {AuthUser, event, fetchAgain, setFetchagain} ) => {
-    const [fetch2, setFetch2] = useState(0)
+const UdalostPreview = ({ AuthUser, event, fetchAgain, setFetchagain }) => {
+    const [fetch2, setFetch2] = useState(0);
 
     useEffect(() => {
-        console.log("v udalost preview ide useeffect")
-        setFetchagain(fetchAgain + 1)
-    }, [fetch2])
+        console.log("v udalost preview ide useeffect");
+        setFetchagain(fetchAgain + 1);
+    }, [fetch2]);
 
-    return ( 
-        <div className="udalosti-preview" key={event.id} >
-            <Link to={`/udalost/${event.id}`}>
-            <p className="Udalost_name">Udalost { event.event_name } </p>
-            <p className="Udalost_lokacia">{ event.location }</p><br /> 
-            <p className="Udalost_zaciatok">O { event.start_date } cas {event.start_time}</p>
-            <p className= "Udalost_kategoria">{ event.kategoria }</p>
-            </Link>
-            <ZaujemButton AuthUser={AuthUser} data={event} id={event.id} fetchAgain={fetchAgain} setFetchagain={setFetchagain} />
-        </div>
-     );
-}
+    return (
+        <table className="udalosti-preview" key={event.id}>
+            <tbody>
+                <tr>
+                        <Link to={`/udalost/${event.id}`}>
+                            <td><div className="Udalost_Info">Udalosť: {event.event_name}</div></td>
+                            <td><div className="Udalost_Info">Lokacia: {event.location}</div></td>
+                            <td><div className="Udalost_Info">
+                                Zaciatok: {event.start_date} <br /> {event.start_time}
+                            </div></td>
+                            <td><div className="Udalost_Info">Kategoria: {event.kategoria}</div></td>
+                        </Link>
+                        </ tr>
+                        <tr>
+                        <div className="ZaujemButton">
+                            <ZaujemButton 
+                                AuthUser={AuthUser}
+                                data={event}
+                                id={event.id}
+                                fetchAgain={fetchAgain}
+                                setFetchagain={setFetchagain}
+                            />
+                        </div>
+                </tr>
+            </tbody>
+        </table>
+    );
+};
  
 export default UdalostPreview;
